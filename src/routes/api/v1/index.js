152849297ac18a749cb0730af003970c61324b47
Ibrahim Saifee <ibrahim.saifee786@gmail.com>
2026-02-20 21:55:24 +0530

    Added login options with kite.

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
 
 const { BaseRoute } = require("@api/base");
-const AuthRoutes = require("./auth");
 const UsersRoutes = require("./users");
 const BacktestRoutes = require("./backtest");
 const KiteRoutes = require("./kite");
 const KotakRoutes = require("./kotak");
+const KotakNeoRoutes = require("./kotakneo");
 
 function V1Routes() {
   BaseRoute.call(this, express.Router());
 
   this.config = () => {
-    this.use("/auth", new AuthRoutes().config());
+    this.use("/kotakneo", new KotakNeoRoutes().config());
+    // this.use("/kite", new KiteRoutes().config());
     this.use("/users", new UsersRoutes().config());
     this.use("/backtest", new BacktestRoutes().config());
     this.use("/kite", new KiteRoutes().config());
